665d5c8388c1bedca98eee86abb45024ec0ad82c
Sergey Chernetsky <chernetsky@gmail.com>
2021-11-24 14:55:41 +0800

    Error catch

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Bot } from 'grammy';
+import { Bot, GrammyError, HttpError } from 'grammy';
 import { getPong } from './utils';
 
 const bot = new Bot(process.env.BOT_TOKEN);
@@ -11,6 +11,19 @@ bot.command('hello', ctx => ctx.reply('World!'));
 bot.hears(/^(ping|пинг|king|кинг)$/i, ctx => ctx.reply(getPong(ctx.match[1])));
 
 // bot.on('message', ctx => ctx.reply(String(new Date())));
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error('Error in request:', e.description);
+  } else if (e instanceof HttpError) {
+    console.error('Could not contact Telegram:', e);
+  } else {
+    console.error('Unknown error:', e);
+  }
+});
 
 bot.start();
 
