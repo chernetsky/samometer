808d928d485d6ee891f6fb71062a50f8bd9d4f27
Sergey Chernetsky <chernetsky@gmail.com>
2025-11-04 14:50:55 +0800

    Update deals.controller.ts

--- src/controllers/deals.controller.ts
+++ src/controllers/deals.controller.ts
@@ -115,14 +115,15 @@ class DealsController {
           .catch(() => { /* Список не поменялся */ });
       } else {
         // Заново создаём сообщение со списком
-        const response = await ctx.api.sendMessage(chatId, text, markup).catch(err => {
+        try {
+          const response = await ctx.api.sendMessage(chatId, text, markup);
+          const { message_id } = response;
+          if (message_id && String(ctx.from.id) === chatId) {
+            // Текущему юзеру записываем новое messageId в сессию
+            ctx.session.messageId = message_id;
+          }
+        } catch (err) {
           console.error(`updateList error: ${err.message}`);
-        });
-
-        const { message_id } = response;
-        if (message_id && String(ctx.from.id) === chatId) {
-          // Текущему юзеру записываем новое messageId в сессию
-          ctx.session.messageId = message_id;
         }
       }
     }
